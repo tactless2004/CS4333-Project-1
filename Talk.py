2373b3c40763e4fa5987f10b4802190f7335598e
tactless2004 <leyton.mckinney@gmail.com>
2025-03-05 13:04:59 -0600

    Implemented Auto Mode. Added some color to the help print.

--- Talk.py
+++ Talk.py
@@ -5,6 +5,7 @@ from multiprocessing import Process
 import os
 import signal
 from psutil import pid_exists # Check if  a Windows Process still exists, useful in input handler logic.
+from colorama import Fore
 
 # CS 4333 Project 1
 
@@ -47,10 +48,10 @@ def print_cmd_usage_warning(warning = "Invalid Arguments") -> None:
 
 # Prints the help message, requested by -help arg
 def print_help_message() -> None:
-    print("Talk.py\n    A commandline peer-to-peer communication app written by Leyton McKinney")
-    print("SERVER MODE\n   usage: python Talk.py -s [-p portnumber]")
-    print("HOST MODE\n   usage: python Talk.py -h [hostname | IPaddress] [-p portnumber]")
-    print("AUTO MODE\n    usage: python Talk.py -a [hostname | IPaddress] [-p portnumber]")
+    print(f"{Fore.RED}Talk.py\n   {Fore.BLUE}A peer-to-peer communication app written by Leyton McKinney\n   for CS4333 Project 1 Spring 2025\n")
+    print(f"{Fore.RED}SERVER MODE\n   {Fore.BLUE}usage: python Talk.py -s [-p portnumber]")
+    print(f"{Fore.RED}HOST MODE\n   {Fore.BLUE}usage: python Talk.py -h [hostname | IPaddress] [-p portnumber]")
+    print(f"{Fore.RED}AUTO MODE\n    {Fore.BLUE}usage: python Talk.py -a [hostname | IPaddress] [-p portnumber]\n{Fore.RESET}")
 
 # Generic warning printer.
 def print_runtime_error(warning = "Talk.py has encountered an unrecoverable issue.") -> None:
@@ -69,7 +70,7 @@ def init_server(ip_address: str, port: int, input_handler: Process) -> None:
 
     while Listening: # Listening mode :)
         if not pid_exists(input_handler.pid): # If we just exitted connecting mode, we need to reinit the nonbound_input_handler
-            input_handler = Process(target = nonbound_input_handler, args=(ip_address, port, is_server))
+            input_handler = Process(target = nonbound_input_handler, args=(ip_address, port, True, False))
             input_handler.start()
 
         client_socket, client_address = server_socket.accept()
@@ -145,6 +146,30 @@ def generic_send(ip_address: str, port: int, client_socket: socket, is_server: b
         elif message:
             client_socket.send(f"{message}".encode())
 
+def init_auto(ip_address: str, port: int, input_handler: Process) -> None:
+    client_socket = socket(AF_INET, SOCK_STREAM)
+    
+    # 1. Try to connect as a client, if this fails, try to connect as a server.
+    try:
+        client_socket.connect((ip_address, port))
+        client_socket.close()
+
+        # 1a. input_handler mode: auto -> client
+        close_input_handler(input_handler)
+        input_handler = Process(target=nonbound_input_handler, args=(ip_address, port, False, False))
+        input_handler.start()
+
+        init_client(ip_address, port, input_handler)
+    
+    # 2. If server DNE, become server.
+    except ConnectionRefusedError:
+        # 2a. input_handler mode: auto -> server 
+        close_input_handler(input_handler)
+        input_handler = Process(target=nonbound_input_handler, args=(ip_address, port, True, False))
+        input_handler.start()
+
+        init_server(ip_address, port, input_handler)
+
 
 def generic_receive(ip_address: str, port: int, client_socket: socket, closed_state: list) -> None:
     Connected = True
@@ -163,16 +188,29 @@ def generic_receive(ip_address: str, port: int, client_socket: socket, closed_st
             Connected = False
         except ConnectionAbortedError:
             Connected = False
-    
-def nonbound_input_handler(ip_address: str, port: int, is_server: bool) -> None:
+
+def close_input_handler(input_handler: Process) -> bool:
+    pid = input_handler.pid
+    if pid_exists(pid):
+        os.kill(pid, signal.SIGINT)
+        return True
+    else:
+        return False
+
+def nonbound_input_handler(ip_address: str, port: int, is_server: bool, auto_mode: bool) -> None:
     sys.stdin = open(0) # Python multiprocessing.Process changes stdin's fd to some other number, need it to be 0
     while True:
         message = sys.stdin.readline()
         if message.strip() == "STATUS":
-            if is_server:
+            if auto_mode:
+                print(f"[STATUS] Client: NONE; Server: NONE\n")
+            elif is_server:
                 print(f"[STATUS] Client: NONE; Server: {ip_address}:{port}\n")
-            else:
+            elif not is_server:
                 print(f"[STATUS] Client: {ip_address}:{port}; Server: NONE\n")
+            else:
+                print(f"Check Semantic Logic, this should be impossible.")
+
         if message.strip() == "QUIT":
             sys.exit() # I don't think this is a project requirement, but it couldn't hurt to process QUIT while not bound on either end.
 
@@ -184,7 +222,9 @@ if __name__ == "__main__":
     args_not_provided = (len(sys.argv) == 0)
     is_server = (sys.argv[0]=="-s") if not args_not_provided else False
     is_client = (sys.argv[0]=='-h') if not args_not_provided else False
+    is_auto = (sys.argv[0]=='-a') if not args_not_provided else False
     is_help = (len(sys.argv) >= 1 and sys.argv[0] == "-help")
+
     if is_server:
         port_provided = (len(sys.argv) == 3) and not args_not_provided and sys.argv[1] == "-p"
         if port_provided:
@@ -192,11 +232,11 @@ if __name__ == "__main__":
                 PORT = int(sys.argv[2])
             except ValueError:
                 print_runtime_error("Invalid Port.")
-        input_handler = Process(target = nonbound_input_handler, args=(LOOPBACK_ADDR, PORT, is_server))
+        input_handler = Process(target = nonbound_input_handler, args=(LOOPBACK_ADDR, PORT, is_server, False))
         input_handler.start()
         init_server(LOOPBACK_ADDR, PORT, input_handler)
 
-    elif is_client:
+    elif is_client or is_auto:
         host_provided = len(sys.argv) >= 2 and sys.argv[1] != "-p"
         LOOPBACK_ADDR = sys.argv[1] if host_provided else LOOPBACK_ADDR
         # Port Logic
@@ -214,17 +254,16 @@ if __name__ == "__main__":
                 PORT = int(sys.argv[2])
             except ValueError:
                 print_runtime_error("Invalid Port.")          
-        input_handler = Process(target = nonbound_input_handler, args=(LOOPBACK_ADDR, PORT, is_server))
-        input_handler.start()
-        init_client(LOOPBACK_ADDR, PORT, input_handler)
+        if is_client:
+            input_handler = Process(target = nonbound_input_handler, args=(LOOPBACK_ADDR, PORT, is_server, False))
+            input_handler.start()
+            init_client(LOOPBACK_ADDR, PORT, input_handler)
+        elif is_auto:
+            input_handler = Process(target = nonbound_input_handler, args=(LOOPBACK_ADDR, PORT, is_server, True))
+            input_handler.start()
+            init_auto(LOOPBACK_ADDR, PORT, input_handler)
+
     elif is_help:
         print_help_message()
     else:
         print("This shouldn't happen, check for semantic error.")
-
-    
-    
-
-    
-    
-
